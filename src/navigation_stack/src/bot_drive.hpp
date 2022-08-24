10b4a5d98719514d244859a15557cf0d9c5ebde0
Harsh Mittal <harshmittal2210@gmail.com>
2022-08-24 21:14:42 +0530

    Auto Mapping Basic script added

---
+++ src/navigation_stack/src/bot_drive.hpp
@@ -0,0 +1,64 @@
+#ifndef BOT_DRIVE_H_
+#define BOT_DRIVE_H_
+
+#include <ros/ros.h>
+
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+
+#define DEG2RAD (M_PI / 180.0)
+#define RAD2DEG (180.0 / M_PI)
+
+#define CENTER 0
+#define LEFT   1
+#define RIGHT  2
+
+#define LINEAR_VELOCITY  0.3
+#define ANGULAR_VELOCITY 1.5
+
+#define GET_TB3_DIRECTION 0
+#define TB3_DRIVE_FORWARD 1
+#define TB3_RIGHT_TURN    2
+#define TB3_LEFT_TURN     3
+
+class BotDrive
+{
+ public:
+  BotDrive();
+  ~BotDrive();
+  bool init();
+  bool controlLoop();
+
+ private:
+  // ROS NodeHandle
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_priv_;
+
+  // ROS Parameters
+
+  // ROS Time
+
+  // ROS Topic Publishers
+  ros::Publisher cmd_vel_pub_;
+
+  // ROS Topic Subscribers
+  ros::Subscriber laser_scan_sub_;
+  ros::Subscriber odom_sub_;
+
+  // Variables
+  double escape_range_;
+  double check_forward_dist_;
+  double check_side_dist_;
+
+  double scan_data_[3] = {0.0, 0.0, 0.0};
+
+  double tb3_pose_;
+  double prev_tb3_pose_;
+
+  // Function prototypes
+  void updatecommandVelocity(double linear, double angular);
+  void laserScanMsgCallBack(const sensor_msgs::LaserScan::ConstPtr &msg);
+  void odomMsgCallBack(const nav_msgs::Odometry::ConstPtr &msg);
+};
+#endif // BOT_DRIVE_H_
